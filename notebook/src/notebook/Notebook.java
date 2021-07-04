976d42e5983791505aeacde2914d8608d26e0c1e
Crovo <2316923313@qq.com>
2021-07-04 10:58:26 +0800

    modify notebook

--- notebook/src/notebook/Notebook.java
+++ notebook/src/notebook/Notebook.java
@@ -1,10 +1,43 @@
 package notebook;
 
-public class Notebook {
+import java.util.ArrayList;
 
+public class Notebook {
+	private ArrayList<String> notes = new ArrayList<String>();
+	
+	public void addNote(String str)
+	{
+		notes.add(str);
+	}
+	
+	public int getSize()
+	{
+		return notes.size();
+	}
+	
+	public String getNote(int index)
+	{
+		return notes.get(index);
+	}
+	
+	public void removeNote(int index)
+	{
+		notes.remove(index);
+	}
+	
+	public String[] listNote()
+	{
+		String str[] = new String[notes.size()];
+		notes.toArray(str);
+		return str;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Notebook nb = new Notebook();
+		nb.addNote("first");
+		nb.addNote("second");
+		System.out.println(nb.getNote(0));
 	}
 
 }

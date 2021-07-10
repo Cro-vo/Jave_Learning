91d2bbc28ee1da0f14f13bd8df07c3602eb68a0f
Crovo <2316923313@qq.com>
2021-07-10 20:19:55 +0800

    增强可扩展性

--- castle/src/castle/Room.java
+++ castle/src/castle/Room.java
@@ -1,28 +1,19 @@
 package castle;
 
+import java.util.HashMap;
+
 public class Room {
     private String description;
-    private Room northExit;
-    private Room southExit;
-    private Room eastExit;
-    private Room westExit;
+    private HashMap<String, Room> exits = new HashMap<String, Room>();
 
     public Room(String description) 
     {
         this.description = description;
     }
 
-    public void setExits(Room north, Room east, Room south, Room west) 
+    public void setExit(String dir, Room room)
     {
-    	
-        if(north != null)
-            northExit = north;
-        if(east != null)
-            eastExit = east;
-        if(south != null)
-            southExit = south;
-        if(west != null)
-            westExit = west;
+    	this.exits.put(dir, room);
     }
 
     @Override
@@ -34,32 +25,18 @@ public class Room {
     public Room getExit(String direction)
     {
     	Room ret = null;
-    	  if(direction.equals("north")) {
-              ret = northExit;
-          }
-          if(direction.equals("east")) {
-        	  ret = eastExit;
-          }
-          if(direction.equals("south")) {
-        	  ret = southExit;
-          }
-          if(direction.equals("west")) {
-        	  ret = westExit;
-          }
+    	ret = exits.get(direction);
     	return ret;
     }
     
     public String getRoomDesc()
     {
     	StringBuilder sb = new StringBuilder("");
-    	 if(northExit != null)
-             sb.append("north ");
-         if(eastExit != null)
-        	 sb.append("east ");
-         if(southExit != null)
-        	 sb.append("south ");
-         if(westExit != null)
-        	 sb.append("west ");
+    	for(String str : exits.keySet())
+    	{
+    		sb.append(str);
+    		sb.append(" ");
+    	}
     	return sb.toString();
     }
     

55cf9fe6cc7bfc91ffe1849ad9b25089f1b11b23
Crovo <2316923313@qq.com>
2021-07-12 17:58:27 +0800

    modi

--- fir proj/src/array.java
+++ fir proj/src/array.java
@@ -1,3 +1,4 @@
+import java.util.*;
 
 public class array {
 
@@ -13,24 +14,64 @@ public class array {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//函数的传递为值传递
 		int a = 5;
 		int b = 10;
 		System.out.println("a = "+ a + ",b = " + b);
 		f(a, b);
 		System.out.println("a = "+ a + ",b = " + b);
 		
+		//foreach取出的是数据的副本
 		int[] c = {1,2,3};
 		for(int i : c)
 		{
-			System.out.println(i);
 			i=0;
 		}
 		
+		//一维数组的输出
 		for(int i : c)
 		{
-			System.out.println(i);
+			System.out.print(i);
+		}
+		System.out.println();
+		
+		//二维数组的输出
+		int arr[][] = new int[][] {{1,2},{3,4,5,6}};
+		for (int x[] : arr)
+		{
+			for (int y : x)
+			{
+				System.out.print(y);
+			}
+			System.out.println();
 		}
 		
+		//填充数组
+		Arrays.fill(c, 6);
+		
+		//数组排序
+		Arrays.sort(c);
+		
+		//数组复制
+		int d[] = Arrays.copyOf(c, 10);
+		for(int x : d)
+		{
+			System.out.print(x);
+		}
+		System.out.println();
+		
+		//数组查找
+		int e[] = new int [] {1,2,5,9,7,3};
+		Arrays.sort(e);
+		for(int x : e)
+		{
+			System.out.print(x);
+		}
+		System.out.println();
+		int index = Arrays.binarySearch(e, 8);
+		System.out.println(index);
+		
+
 	}
 
 }
